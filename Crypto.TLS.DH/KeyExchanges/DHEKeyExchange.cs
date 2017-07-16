f19762de86db12f77c5942432fc318bb622cb80c
Will Smith <will@toxon.co.uk>
2017-07-16 14:27:11 +0100

    Pulled out common bits of DH exchanges

--- Crypto.TLS.DH/KeyExchanges/DHEKeyExchange.cs
+++ Crypto.TLS.DH/KeyExchanges/DHEKeyExchange.cs
@@ -1,29 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Numerics;
-using Crypto.Certificates;
 using Crypto.Core.Randomness;
-using Crypto.RSA.Keys;
 using Crypto.TLS.Config;
 using Crypto.TLS.DH.Config;
-using Crypto.TLS.DH.Keys;
 using Crypto.TLS.KeyExchange;
 using Crypto.TLS.Messages.Handshakes;
-using Crypto.Utils;
-using Crypto.Utils.IO;
 
 namespace Crypto.TLS.DH.KeyExchanges
 {
-    public class DHEKeyExchange : IKeyExchange
+    public class DHEKeyExchange : DHKeyExchangeBase
     {
         private readonly IServiceProvider _serviceProvider;
         
         private readonly IRandom _random;
-        private readonly MasterSecretCalculator _masterSecretCalculator;
 
         private readonly DHParameterConfig _dhParameterConfig;
         private readonly DHExchangeConfig _dhExchangeConfig;
-        private readonly CertificateConfig _certificateConfig;
 
         public DHEKeyExchange(
             IServiceProvider serviceProvider,
@@ -34,69 +27,34 @@ namespace Crypto.TLS.DH.KeyExchanges
             DHParameterConfig dhParameterConfig,
             DHExchangeConfig dhExchangeConfig,
             CertificateConfig certificateConfig)
+                :base(masterSecretCalculator, certificateConfig)
         {
             _serviceProvider = serviceProvider;
             
             _random = random;
-            _masterSecretCalculator = masterSecretCalculator;
 
             _dhParameterConfig = dhParameterConfig;
             _dhExchangeConfig = dhExchangeConfig;
-            _certificateConfig = certificateConfig;
         }
-
-        public bool IsCompatible(CipherSuite cipherSuite, X509Certificate certificate)
+        
+        public override IEnumerable<HandshakeMessage> GenerateHandshakeMessages()
         {
-            // TODO check cipherSuite == RSA/DSS
-            // cert signed with RSA
-            if (!RSAKeyReader.IsRSAIdentifier(certificate.SignatureAlgorithm.Algorithm))
+            foreach (var message in base.GenerateHandshakeMessages())
             {
-                return false;
+                yield return message;
             }
-
-            // cert has RSA public key
-            if (!(certificate.SubjectPublicKey is RSAPublicKey))
-            {
-                return false;
-            }
-
-            // TODO ?
-            return true;
-        }
-
-        public IEnumerable<HandshakeMessage> GenerateHandshakeMessages()
-        {
-            // TODO should check CipherSuite & Certificate type (RSA, DSS) match
-
+            
             // 512 is "approx" 256-bits of security
             _dhExchangeConfig.X = _random.RandomBig(256);
 
-            var serverY = BigInteger.ModPow(_dhParameterConfig.G, _dhExchangeConfig.X, _dhParameterConfig.P);
+            var ys = BigInteger.ModPow(_dhParameterConfig.G, _dhExchangeConfig.X, _dhParameterConfig.P);
 
-            yield return new CertificateMessage(_certificateConfig.CertificateChain);
-            yield return new ServerKeyExchangeMessage(_serviceProvider, _dhParameterConfig.P, _dhParameterConfig.G, serverY);
+            yield return new ServerKeyExchangeMessage(_serviceProvider, _dhParameterConfig.P, _dhParameterConfig.G, ys);
         }
 
-        public void HandleClientKeyExchange(ClientKeyExchangeMessage message)
+        public override BigInteger CalculatedSharedSecret(BigInteger yc)
         {
-            var yc = ReadMessage(message);
-
-            var sharedSecret = BigInteger.ModPow(yc, _dhExchangeConfig.X, _dhParameterConfig.P);
-            var preMasterSecret = sharedSecret.ToByteArray(Endianness.BigEndian);
-
-            var masterSecret = _masterSecretCalculator.Compute(preMasterSecret);
-            _masterSecretCalculator.ComputeKeysAndUpdateConfig(masterSecret);
-        }
-
-        private static BigInteger ReadMessage(ClientKeyExchangeMessage message)
-        {
-            var length = EndianBitConverter.Big.ToUInt16(message.Body, 0);
-            SecurityAssert.Assert(message.Body.Length == length + 2);
-
-            var param = new byte[length];
-            Array.Copy(message.Body, 2, param, 0, length);
-
-            return param.ToBigInteger(Endianness.BigEndian);
+            return BigInteger.ModPow(yc, _dhExchangeConfig.X, _dhParameterConfig.P);
         }
     }
 }
--- Crypto.TLS.DH/KeyExchanges/DHKeyExchange.cs
+++ Crypto.TLS.DH/KeyExchanges/DHKeyExchange.cs
@@ -1,76 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Numerics;
+﻿using System.Numerics;
 using Crypto.Certificates;
-using Crypto.RSA.Keys;
 using Crypto.TLS.Config;
 using Crypto.TLS.DH.Keys;
 using Crypto.TLS.KeyExchange;
-using Crypto.TLS.Messages.Handshakes;
 using Crypto.Utils;
-using Crypto.Utils.IO;
 
 namespace Crypto.TLS.DH.KeyExchanges
 {
-    public class DHKeyExchange : IKeyExchange
+    public class DHKeyExchange : DHKeyExchangeBase
     {
         private readonly CertificateManager _certificateManager;
-        private readonly MasterSecretCalculator _masterSecretCalculator;
-
-        private readonly CertificateConfig _certificateConfig;
 
         public DHKeyExchange(
             CertificateManager certificateManager,
             MasterSecretCalculator masterSecretCalculator,
 
             CertificateConfig certificateConfig)
+                : base(masterSecretCalculator, certificateConfig)
         {
             _certificateManager = certificateManager;
-            _masterSecretCalculator = masterSecretCalculator;
-
-            _certificateConfig = certificateConfig;
         }
 
-        public bool IsCompatible(CipherSuite cipherSuite, X509Certificate certificate)
-        {
-            // TODO check cipherSuite == RSA/DSS
-            // cert signed with RSA
-            if (!RSAKeyReader.IsRSAIdentifier(certificate.SignatureAlgorithm.Algorithm))
-            {
-                return false;
-            }
-
-            // cert has DH public key
-            if (!(certificate.SubjectPublicKey is DHPublicKey))
-            {
-                return false;
-            }
-
-            // TODO ?
-            return true;
-        }
-
-        public IEnumerable<HandshakeMessage> GenerateHandshakeMessages()
-        {
-            yield return new CertificateMessage(_certificateConfig.CertificateChain);
-        }
-
-        public void HandleClientKeyExchange(ClientKeyExchangeMessage message)
+        public override BigInteger CalculatedSharedSecret(BigInteger yc)
         {
             var key = GetPrivateKey();
-
-            var yc = ReadMessage(message);
-
-            var sharedSecret = BigInteger.ModPow(yc, key.X, key.DHPublicKey.P);
-            var preMasterSecret = sharedSecret.ToByteArray(Endianness.BigEndian);
-
-            var masterSecret = _masterSecretCalculator.Compute(preMasterSecret);
-            _masterSecretCalculator.ComputeKeysAndUpdateConfig(masterSecret);
+            
+            return BigInteger.ModPow(yc, key.X, key.DHPublicKey.P);
         }
-
+        
         private DHPrivateKey GetPrivateKey()
         {
-            var cert = _certificateConfig.Certificate;
+            var cert = CertificateConfig.Certificate;
             var key = _certificateManager.GetPrivateKey(cert.SubjectPublicKey);
 
             var dhKey = key as DHPrivateKey;
@@ -79,15 +39,6 @@ namespace Crypto.TLS.DH.KeyExchanges
             return dhKey;
         }
 
-        private static BigInteger ReadMessage(ClientKeyExchangeMessage message)
-        {
-            var length = EndianBitConverter.Big.ToUInt16(message.Body, 0);
-            SecurityAssert.Assert(message.Body.Length == length + 2);
 
-            var param = new byte[length];
-            Array.Copy(message.Body, 2, param, 0, length);
-
-            return param.ToBigInteger(Endianness.BigEndian);
-        }
     }
 }
